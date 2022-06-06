a53abab9a8edab590f4d99f79d0d501e468334b6
Gukapriyan <88232274+Gukapriyan@users.noreply.github.com>
2022-06-06 22:37:13 +0530

    Add files via upload

---
+++ ElementVisible.java
@@ -0,0 +1,27 @@
+package week5.day5;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ElementVisible {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/appear.html");
+		driver.manage().window().maximize();
+		WebElement ele =driver.findElement(By.id("btn"));
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.visibilityOf(ele));
+		System.out.println(ele.isDisplayed());
+
+	}
+
+}
